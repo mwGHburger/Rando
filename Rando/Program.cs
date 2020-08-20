2c17ef567d19cf33663feef2588fdba33929eb39
Michael Wang <michael.wang@myob.com>
2020-08-20 15:52:43 +1000

    test2

--- Rando/Program.cs
+++ Rando/Program.cs
@@ -6,7 +6,7 @@ namespace Rando
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! galerhbgaejrhg");
+            Console.WriteLine("Hello World! New");
         }
     }
 }
